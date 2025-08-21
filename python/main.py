d4aba823c0a38ca97c29ff67bdafe3e50a55a5e1
Peter Schramm <ptsc4816@hochschule-trier.de>
2025-08-21 15:58:13 +0200

    Added python setup

---
+++ python/main.py
@@ -0,0 +1,49 @@
+import threading, time, queue
+from vicon_dssdk.ViconDataStream import Client
+
+client = Client()
+host = "localhost:801"
+
+client.Connect(host)
+client.SetStreamMode(Client.StreamMode.EServerPush)
+
+# Set proper orientation
+client.SetAxisMapping(Client.AxisMapping.ELeft, Client.AxisMapping.EUp, Client.AxisMapping.EForward)
+
+# Enable necessary channels
+client.EnableLightweightSegmentData()
+client.EnableSegmentData()
+
+# Setup logging frequency
+logEveryNFrames = 20
+frameCounter = 0
+
+# Start a Thread to read strings for logging
+log_queue = queue.Queue()
+def log_worker():
+    while True:
+        msg = log_queue.get()
+        if msg is None:
+            break  # exit signal
+        print(msg)
+        log_queue.task_done()
+
+threading.Thread(target=log_worker, daemon=True).start()
+
+while True:
+    # Start timer
+    start = time.time()
+
+    # Should be blocking:
+    result = client.GetFrame()
+    if not result:
+        continue
+
+    # On success get some data and stop timer
+    frameNumber = client.GetFrameNumber()
+    end = time.time()
+    elapsed = (end - start) * 1000
+
+    frameCounter += 1
+    if (frameCounter % logEveryNFrames == 0):
+        log_queue.put(f"Tick at frame {frameNumber}, took {elapsed:.2f} ms")
