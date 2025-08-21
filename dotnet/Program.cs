83660e1bef07ac001b157416d2a379d76e84b40f
Peter Schramm <ptsc4816@hochschule-trier.de>
2025-08-21 15:58:22 +0200

    minor adjustments

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -11,44 +11,44 @@ client.SetStreamMode(StreamMode.ServerPush);
 // Set proper orientation
 client.SetAxisMapping(Direction.Left, Direction.Up, Direction.Forward);
 
-// Enable all the data types
-// client.EnableLightweightSegmentData();
-// client.EnableSegmentData();
-client.EnableMarkerData();
-// client.EnableUnlabeledMarkerData();
-// client.EnableMarkerRayData();
-// client.EnableDeviceData();
-// client.EnableCentroidData();
+// Enable necessary channels
+client.EnableLightweightSegmentData();
+client.EnableSegmentData();
 
-int logEveryNFrames = 20; // log once per second at 200Hz
+// Setup logging frequency
+int logEveryNFrames = 20;
 int frameCounter = 0;
 
+// Start a Thread to read strings for logging
 var logQueue = Channel.CreateUnbounded<string>();
-
 _ = Task.Run(async () =>
 {
-    await foreach (var message in logQueue.Reader.ReadAllAsync())
-    {
-        Console.WriteLine(message);
-    }
+  await foreach (var message in logQueue.Reader.ReadAllAsync())
+  {
+    Console.WriteLine(message);
+  }
 });
 
 while (true)
 {
+  // Start timer
   var sw = Stopwatch.StartNew();
-  // should be blocking:
+
+  // Should be blocking:
   var result = client.GetFrame().Result;
   if (result == Result.NoFrame)
   {
     continue;
   }
+
+  // On success get some data and stop timer
   var frameNumber = client.GetFrameNumber().FrameNumber;
-  // Console.WriteLine($"{result}: Got a frame {frameNumber}");
   sw.Stop();
-  // Console.WriteLine($"Loop took {sw.ElapsedMilliseconds} ms");
+
+  // Send a log every nth frame
   if (++frameCounter % logEveryNFrames == 0)
   {
-    logQueue.Writer.TryWrite($"Tick at frame {frameNumber}, took {sw.ElapsedMilliseconds}ms");
+    logQueue.Writer.TryWrite($"Tick at frame {frameNumber}, took {sw.ElapsedMilliseconds} ms");
   }
 }
 
